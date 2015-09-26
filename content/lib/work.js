9813bab94ea8869bb808ab5e9075bc59cd32ce22
Olivier Breuleux <breuleux@gmail.com>
2015-09-26 00:28:14 -0400

    load a smaller highlight.js

--- content/lib/work.js
+++ content/lib/work.js
@@ -1,11 +1,14 @@
 
 importScripts('jspm_packages/system.js', 'config.js');
 
+window = self;
+importScripts('highlight.pack.js');
+
 console.log("worker loaded");
 
 var u = null;
 
-System.import("edit2").then(function (m) {
+System.import("edit").then(function (m) {
     console.log("quaint loaded");
     u = m.UpdaterWorker();
     u.work();
